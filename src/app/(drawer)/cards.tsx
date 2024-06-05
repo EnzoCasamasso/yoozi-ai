c003f127a85d48b94831c65b5f198bfcdb8c99d9
Enzo <enzocasamasso@gmail.com>
2024-06-05 20:29:47 -0300

    feat: add root drawer layout

---
+++ src/app/(drawer)/cards.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "tamagui";
+
+export default function CardsPage() {
+  return (
+    <View
+      flex={1}
+      ai={'center'}
+      jc={'center'}>
+      <Text>CardsPage</Text>
+    </View>
+  )
+}
